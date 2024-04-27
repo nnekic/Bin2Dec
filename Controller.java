6bc99bbdf391560f7ec22ed63a8836c9a3f59533
nemanja <95zakon@gmail.com>
2024-04-27 23:57:25 +0200

    Groundwork done, tomorrow will complete project

--- Controller.java
+++ Controller.java
@@ -1,5 +1,37 @@
 package application;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller{
 	
+	@FXML
+	private TextField TextFiledBinary;
+	
+	@FXML
+	private Button ConvertButton;
+	
+	@FXML
+	private TextField DecimalTextField;
+	
+	public void Convert(ActionEvent event) {
+		try {
+		int binaryNumber = Integer.parseInt(TextFiledBinary.getText());
+		if(binaryNumber != 1 || binaryNumber != 0) {
+			TextFiledBinary.setText("Please use numbers 1 or 0");
+		}
+		}
+		catch(NumberFormatException e) {
+			TextFiledBinary.setText("Please use numbers 1 or 0");
+		}
+		catch(Exception e) {
+			System.out.println("error");
+		}
+	}
 }
+		
+	
+
